dcfff277af3b08e0f4be07a4fbc1c48e08316964
alexandra-grigore <51346201+alexandra-grigore@users.noreply.github.com>
2019-07-07 01:20:10 -0400

    Updated search function

--- TemplateEditor/src/data/properties.jsx
+++ TemplateEditor/src/data/properties.jsx
@@ -1,4 +1,5 @@
 export const baseURL = 'http://ocp-apps-elb-811293208.us-east-2.elb.amazonaws.com:8082/fhir/baseDstu3/StructureDefinition/';
+export const baseURLNext = 'http://ocp-apps-elb-811293208.us-east-2.elb.amazonaws.com:8082/fhir/baseDstu3/';
 
 //export const baseURL = 'http://hapi.fhir.org/baseR4/StructureDefinition/';
 //export const inserURL = "http://hapi.fhir.org/baseR4/StructureDefinition?_format=json";
