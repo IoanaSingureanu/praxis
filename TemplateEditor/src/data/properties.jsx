66f49497d97d4ef52f61d2c4aa5b64601f032747
Ioana Singureanu <ioana.singureanu@gmail.com>
2019-07-12 16:06:47 -0400

    Updated comments

--- TemplateEditor/src/data/properties.jsx
+++ TemplateEditor/src/data/properties.jsx
@@ -1,10 +1,9 @@
-// FHIM server StructureDefinition server
+// FHIM server StructureDefinition resource URL
 export const baseURL =     'http://ocp-apps-elb-811293208.us-east-2.elb.amazonaws.com:8082/fhir/baseDstu3/StructureDefinition/';
 // FHIR server  base url
 export const baseURLNext = 'http://ocp-apps-elb-811293208.us-east-2.elb.amazonaws.com:8082/fhir/baseDstu3/';
 // FHIM Profile Genenrator Function id - ths data is inserted to URL
 export const generateFunctionId = '';
-
 // Search dashborard - the number of matching structurues per page
 export const structurePageCount = 10;
 // Template editor - data elemente per page
