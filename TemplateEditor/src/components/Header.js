4ebcf4df77d97362a9a6054118d6775a68d34365
alexandra-grigore <51346201+alexandra-grigore@users.noreply.github.com>
2019-08-10 18:48:58 -0400

    Changed App Name

--- TemplateEditor/src/components/Header.js
+++ TemplateEditor/src/components/Header.js
@@ -8,9 +8,9 @@ export const Header = ({ startLogout }) => (
     <div className="content-container">
       <div className="header__content">
         <Link className="header__title" to="/dashboard">
-          <h1>Praxis Editor</h1>
+          <h2>Template Editor</h2>
         </Link>
-        <button className="button button--link" onClick={startLogout}>Logout</button>
+        <button className="button button--link" onClick={startLogout}>Logout</button> 
       </div>
     </div>
   </header>
--- TemplateEditor/src/firebase/firebase.js
+++ TemplateEditor/src/firebase/firebase.js
@@ -1,17 +1,17 @@
 import * as firebase from 'firebase';
 
 // Web app's Firebase configuration
-const config = {
-  apiKey: "AIzaSyAyw3mwDprDZbrawMOpJuOk6R6dg2lFszE",
-  authDomain: "fhim-2de9c.firebaseapp.com",
-  databaseURL: "https://fhim-2de9c.firebaseio.com",
-  projectId: "fhim-2de9c",
-  storageBucket: "fhim-2de9c.appspot.com",
-  messagingSenderId: "628713125497",
-  appId: "1:628713125497:web:9a2686aa4ceaeaf6"
+var firebaseConfig = {
+    apiKey: "AIzaSyAyw3mwDprDZbrawMOpJuOk6R6dg2lFszE",
+    authDomain: "fhim-2de9c.firebaseapp.com",
+    databaseURL: "https://fhim-2de9c.firebaseio.com",
+    projectId: "fhim-2de9c",
+    storageBucket: "fhim-2de9c.appspot.com",
+    messagingSenderId: "628713125497",
+    appId: "1:628713125497:web:9a2686aa4ceaeaf6"
 };
 // Initialize Firebase
-firebase.initializeApp(config);
+firebase.initializeApp(firebaseConfig);
 
 const database = firebase.database();
 const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
